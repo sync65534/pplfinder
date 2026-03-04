9024151d77d384643fab0f54e326e112466a6190
sync65534 <142959904+sync65534@users.noreply.github.com>
2026-03-04 01:40:12 -0500

    complete redoing of workflow logic

--- pplfinder.py
+++ pplfinder.py
@@ -1,52 +1,27 @@
-import requests
-import pandas as pd
-import pprint
-import csv
 from bs4 import BeautifulSoup
-import re
+import json
+import extractions
+import requests
+import county
 
+def load_county_list(fname="/home/tris/stalking/county_list.json"):
+    a = []
+    with open(fname, 'r') as f:
+        data = json.load(f)
+        for county_name, info in data.items():
+            state, data_type, links, notes = info.values()
+            print(links)
+            a.append(county.County(county_name, state, data_type, links, notes))
+    return a
+county_list = load_county_list()
 
-class county:
-    def __init__(self, name, state, link, datacase, notes):
-        self.name = name
-        self.state = state
-        self.link = link
-        self.datacase = datacase
-        self.notes = notes
-    def __repr__(self):
-        return self.name
-# make and fill county list
-county_list = []
-
-with open("county_list.csv", 'r', encoding="utf-8") as f:
-    reader = csv.reader(f)
-    for line in reader:
-        name, state, link, datacase, notes = line
-        county_list.append(county(name, state, link, datacase, notes)) 
-
-# def data cases
-def extract_cdata_content(text):
-    cdata_match = re.search(r"<!\[CDATA\[(.*?)\]\]>", text, re.DOTALL)
-    html_block = cdata_match.group(1)
-
-    soup = BeautifulSoup(html_block, "html.parser")
-
-    names = [
-        div.get_text(strip=True)
-        for div in soup.find_all("div", class_="secondaryResultItem")
-    ]
-    names = [ name for name in names if name.find("Owner Address") == -1 and name.find("Property Address") == -1]
-    return names
-case_dict = {
-    "CDATA": extract_cdata_content
+data_cases = {
+    "WTHGIS": extractions.WTHGIS_extract
 }
-
-# func to find the thingy
 def find(name):
+    collective = {}
     for county in county_list:
-        link = county.link
-        link = link.replace("NAMEHERE", name)
-        r = requests.get(link)
-    print(link)
-    return case_dict.get(county.datacase)(r.content.decode())
-
+        link = county.links[0].replace("NAMEHERE", name)
+        resp = requests.get(link).content.decode()
+        collective.update(data_cases.get(county.datacase)(resp, county))
+    return json.dumps(collective)
