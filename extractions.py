ee6e613a5405476847729b9969fb722dbdf3ec16
sync65534 <142959904+sync65534@users.noreply.github.com>
2026-03-06 00:01:37 -0500

    Fix regex pattern to capture names correctly because indianna sucks

--- extractions.py
+++ extractions.py
@@ -4,15 +4,21 @@ def WTHGIS_extract(responce, county):
     cdata_match = re.search(r"<!\[CDATA\[(.*?)\]\]>", responce, re.DOTALL)
     if not cdata_match:
         return [], []
+
     html_fragment = cdata_match.group(1)
+    if "No matches found" in html_fragment:
+        return 
+
     pattern = re.compile(
         r"showf\(\s*\d+\s*,\s*(\d+)\s*\)"   # capture X
         r".*?"
-        r"<div\s+class='secondaryResultItem'>([^<]+)</div>",  # capture name
+        r"<div\s+class='secondaryResultItem'>([^\d<]+)</div>",  # capture name
         re.DOTALL
     )
     matches = pattern.findall(html_fragment)
+
     results = {name.strip(): county.links[1].replace("VALUEHERE", str(int(x))) for x, name in matches}
+
     return results
 
 
